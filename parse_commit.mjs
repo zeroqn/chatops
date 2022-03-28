038feae6d36b09c6c27fcd93af50e68d11e40051
zero.qn <zero.qn@outlook.com>
2022-03-28 17:34:42 +0800

    feat: fetch prebuilds component info

--- parse_commit.mjs
+++ parse_commit.mjs
@@ -6,7 +6,27 @@ const octokit = new Octokit({
   auth: process.env.GITHUB_AUTH_TOKEN,
 });
 
+const getPrebuilds = async (org, page, perPage) => {
+  return await octokit.rest.packages.getAllPackageVersionsForPackageOwnedByOrg({
+    package_type: "container",
+    package_name: "godwoken-prebuilds",
+    org,
+    page,
+    per_page: perPage,
+  });
+};
+
+const getPrebuildsByTag = async (org, tag) => {
+  const pkgs = await getPrebuilds(org, 1, 100);
+  for (const pkg of pkgs.data) {
+    if (pkg.metadata.container.tags.includes(tag)) {
+      return pkg;
+    }
+  }
+};
+
 const commentBody = `
+prebuilds: dev-202203280240
 godwoken: develop
 scripts: 81676d9d53ffdf5bbaa60483928d07da16eb4a88
 polyjuice: e37553b9
@@ -14,6 +34,30 @@ polyjuice: e37553b9
 console.log(`comment: ${commentBody}`);
 
 // Parse commentBody
+const prebuilds = {
+  org: "nervosnetwork",
+  repo: "godwoken-docker-prebuilds",
+  pattern: /prebuilds: (.*)/,
+  packageType: "container",
+  packageName: "godwoken-prebuilds",
+  tags: undefined,
+  sha: undefined,
+  htmlUrl: undefined,
+  manifest: {
+    godwoken: {
+      pattern: /&quot;ref.component.godwoken-sha1&quot;: &quot;(.{40})&quot;,/,
+      sha: undefined,
+    },
+    scripts: {
+      pattern: /&quot;ref.component.godwoken-scripts-sha1&quot;: &quot;(.{40})&quot;,/,
+      sha: undefined,
+    },
+    polyjuice: {
+      pattern: /&quot;ref.component.godwoken-polyjuice-sha1&quot;: &quot;(.{40})&quot;,/,
+      sha: undefined,
+    },
+  },
+};
 const components = {
   godwoken: {
     owner: "nervosnetwork",
@@ -62,12 +106,48 @@ const components = {
   },
 };
 
+// Fetch prebuilds
+const matchPrebuilds = async () => {
+  const match = prebuilds.pattern.exec(`${commentBody}`);
+  if (match) {
+    return await getPrebuildsByTag(prebuilds.org, match[1]);
+  } else {
+    return (await getPrebuilds(prebuilds.org, 1, 2)).data[0];
+  }
+};
+const pkg = await matchPrebuilds();
+
+prebuilds.tags = pkg.metadata.container.tags;
+prebuilds.htmlUrl = pkg.html_url;
+prebuilds.sha = pkg.name;
+console.log(`pkg: ${JSON.stringify(pkg)}`);
+
+// Fetch prebuilds components commit;
+const manifestPage = JSON.stringify(
+  await octokit.request(`GET ${prebuilds.htmlUrl}`)
+);
+for (const name in prebuilds.manifest) {
+  const label = prebuilds.manifest[name];
+  const match = label.pattern.exec(manifestPage);
+  if (match) {
+    label.sha = match[1];
+  } else {
+    throw `${name}-sha1 not found`;
+  }
+}
+console.log(`${JSON.stringify(prebuilds)}`);
+
 for (const name in components) {
   const comp = components[name];
   const match = comp.pattern.exec(`${commentBody}`);
   if (match) {
     comp.branchOrCommit = match[1];
+  } else if (prebuilds.manifest[name]) {
+    comp.branchOrCommit = prebuilds.manifest[name].sha;
   } else {
+    if (name === "web3") {
+      continue;
+    }
     comp.branchOrCommit = comp.branch;
   }
 }
@@ -77,6 +157,11 @@ console.log(JSON.stringify(components));
 for (const name in components) {
   const comp = components[name];
   console.log(`fetch branch or commit ${name}`);
+
+  if (comp.branchOrCommit === undefined) {
+    console.log(`skip fetch component info ${name}`);
+    continue;
+  }
 
   try {
     // Try branch
@@ -136,9 +221,14 @@ console.log(JSON.stringify(components));
 // Post integration test info
 let componentInfo = `
 Run integration with following specific components:
+
+prebuilds: [${prebuilds.tags}](${prebuilds.htmlUrl})
 `;
 for (const name in components) {
   const comp = components[name];
+  if (comp.commit === undefined) {
+    continue;
+  }
   const shortSha = comp.commit.substr(0, 7);
   componentInfo = `${componentInfo}\n${name}: [${shortSha}](${comp.htmlUrl})`;
 }
